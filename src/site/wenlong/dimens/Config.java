47a431d11289b07bc08ad59e9884ef5192f161b0
Gavin <guowenlong20000@sina.com>
2018-10-29 08:02:48 +0800

    develop : 插件GUI完成

--- src/site/wenlong/dimens/Config.java
+++ src/site/wenlong/dimens/Config.java
@@ -3,6 +3,6 @@ package site.wenlong.dimens;
 public class Config {
     public final static String DEFAULT_FILE_NAME = "dimens.xml";
     public final static String PLUGINS_NAME = "Dimens Generating Tools";
-    public static final boolean IS_COVER_FILE = false;
-    public static final float DEFAULT_ORIGIN_DIMENS = 360F;
+    public static boolean IS_COVER_FILE = false;
+    public static float DEFAULT_ORIGIN_DIMENS = 360F;
 }
