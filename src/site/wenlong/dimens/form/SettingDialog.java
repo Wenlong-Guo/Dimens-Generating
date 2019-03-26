91a7113905c86d37a0fffef9aea79d85f8b9ee96
江玉环 <jiangyuhuan@gtexpress.cn>
2019-03-26 09:18:03 +0800

    重构UI一部分

---
+++ src/site/wenlong/dimens/form/SettingDialog.java
@@ -0,0 +1,18 @@
+package site.wenlong.dimens.form;
+
+import javax.swing.*;
+
+public class SettingDialog {
+    private JPanel panel;
+    private JCheckBox overwriteExistingFilesCheckBox;
+    private JCheckBox keepTheDecimalPointCheckBox;
+    private JButton button1;
+    private JCheckBox setTheMinimumWidthCheckBox;
+    private JCheckBox checkBox4;
+    private JComboBox comboBox1;
+    private JRadioButton radioButton1;
+    private JRadioButton radioButton2;
+    private JButton button2;
+    private JTextField textField1;
+    private JTextField textField2;
+}
