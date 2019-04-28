9e1c64c5f59787515953d1127a0cc78f3effcd6f
郭文龙 <guowenlong@gtexpress.cn>
2019-04-28 16:12:22 +0800

    重绘UI完成

--- src/site/wenlong/dimens/form/SettingDialog.java
+++ src/site/wenlong/dimens/form/SettingDialog.java
@@ -6,13 +6,13 @@ public class SettingDialog {
     private JPanel panel;
     private JCheckBox overwriteExistingFilesCheckBox;
     private JCheckBox keepTheDecimalPointCheckBox;
-    private JButton button1;
+    private JButton moreButton;
     private JCheckBox setTheMinimumWidthCheckBox;
-    private JCheckBox checkBox4;
+    private JCheckBox setTheQualifierNameCheckBox;
     private JComboBox comboBox1;
-    private JRadioButton radioButton1;
-    private JRadioButton radioButton2;
-    private JButton button2;
-    private JTextField textField1;
+    private JButton generatingDimensButton;
     private JTextField textField2;
+    private JRadioButton singleRadioButton;
+    private JTextField textField1;
+    private JButton advancedOptionButton;
 }
