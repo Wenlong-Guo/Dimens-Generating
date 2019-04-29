b354be2a9f61ea9735a49fe0df4a5f56d0de79f3
郭文龙 <guowenlong@gtexpress.cn>
2019-04-29 18:33:18 +0800

    提交一部分Configuration

--- src/site/wenlong/dimens/core/Configuration.java
+++ src/site/wenlong/dimens/core/Configuration.java
@@ -1,12 +1,12 @@
 package site.wenlong.dimens.core;
 
 import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.State;
 import com.intellij.util.xmlb.XmlSerializerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-
-
+@State(name = "Configuration", storages = {@com.intellij.openapi.components.Storage(value = "$APP_CONFIG$/Configuration.xml")})
 public class Configuration implements PersistentStateComponent<Configuration> {
     private boolean isCover;
     private boolean isKeepPoint;
@@ -14,6 +14,11 @@ public class Configuration implements PersistentStateComponent<Configuration> {
     private boolean isReName;
     private boolean isSingle;
     private boolean isMultiple;
+    private String mBit = "2";
+    private String mOriginWidth = "360";
+    private String mRename = "sw";
+    private String mSingle = "400";
+    private String mMulitple = "300,320,340,360,380,400,420,440,460,480,500";
 
     @Nullable
     @Override
@@ -23,7 +28,7 @@ public class Configuration implements PersistentStateComponent<Configuration> {
 
     @Override
     public void loadState(@NotNull Configuration element) {
-        XmlSerializerUtil.copyBean(element,this);
+        XmlSerializerUtil.copyBean(element, this);
     }
 
     @Override
